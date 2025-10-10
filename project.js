8f8a7e1c6aff72563fbd13eec16f65b649e61d46
Xcode <Orjikeyz7@gmail.com>
2025-10-10 10:36:18 +0100

    added nigeria business portal

--- project.js
+++ project.js
@@ -1,36 +1,57 @@
-const projects = [{
-    name: "HAPPINESS INSIDE ME",
-    description: "Happiness Inside Me is a serene retreat nestled in the Lasne countryside, just 20 minutes from Brussels. It offers transformative workshops, meditations, and coaching sessions aimed at unlocking inner joy and personal growth. Whether in group settings or individual guidance, participants embark on journeys of self-discovery and empowerment. The retreat fosters a nurturing environment where individuals can reconnect with themselves and find lasting peace",
-    tags: ["Html", "Css", "Javascript"],
-    imgUrl: "./images/Happiness.png",
-    url: "https://happinesssinsideme.vercel.app/"
+const projects = [
+  {
+    name: 'HAPPINESS INSIDE ME',
+    description:
+      'Happiness Inside Me is a serene retreat nestled in the Lasne countryside, just 20 minutes from Brussels. It offers transformative workshops, meditations, and coaching sessions aimed at unlocking inner joy and personal growth. Whether in group settings or individual guidance, participants embark on journeys of self-discovery and empowerment. The retreat fosters a nurturing environment where individuals can reconnect with themselves and find lasting peace',
+    tags: ['Html', 'Css', 'Javascript'],
+    imgUrl: './images/Happiness.png',
+    url: 'https://happinesssinsideme.vercel.app/'
   },
   {
-    name: "Atlantico Business Development",
-    description: "Atlantico Business Development offers specialized services in the maritime sector, focusing on international business development across Atlantic markets. Based in Rotterdam, they provide expertise in maritime security, ship registration, port development, and market analysis. Their dedicated team collaborates with a network of consultants in countries like Portugal, Brazil, Angola, Spain, the UK, and the Netherlands. Atlantico aims to support private sector clients with strategic insights and innovative solutions in the maritime industry.",
-    tags: ["Html", "Css", "Javascript"],
-    imgUrl: "./images/Atlantico Business.png",
-    url: "https://atlanticobusinessdevelopments.vercel.app/"
+    name: 'Atlantico Business Development',
+    description:
+      'Atlantico Business Development offers specialized services in the maritime sector, focusing on international business development across Atlantic markets. Based in Rotterdam, they provide expertise in maritime security, ship registration, port development, and market analysis. Their dedicated team collaborates with a network of consultants in countries like Portugal, Brazil, Angola, Spain, the UK, and the Netherlands. Atlantico aims to support private sector clients with strategic insights and innovative solutions in the maritime industry.',
+    tags: ['Html', 'Css', 'Javascript'],
+    imgUrl: './images/Atlantico Business.png',
+    url: 'https://atlanticobusinessdevelopments.vercel.app/'
   },
   {
-    name: "EchoVibe Contact Gain",
-    description: "EchoVibe is a smart contact exchange platform that allows users to share and collect WhatsApp contacts seamlessly with just one tap—no need to save manually or speak. It supports offline mode, one-click contact export to phonebooks, and encourages daily networking via a community-driven WhatsApp group.",
-    tags: ["Html", "Css", "Javascript", "Vercel", "Supabase", "Nodejs", "Vercel Serverless Functions"],
-    imgUrl: "./images/echovibe.png",
-    url: "https://echovibe.vercel.app/"
+    name: 'EchoVibe Contact Gain',
+    description:
+      'EchoVibe is a smart contact exchange platform that allows users to share and collect WhatsApp contacts seamlessly with just one tap—no need to save manually or speak. It supports offline mode, one-click contact export to phonebooks, and encourages daily networking via a community-driven WhatsApp group.',
+    tags: [
+      'Html',
+      'Css',
+      'Javascript',
+      'Vercel',
+      'Supabase',
+      'Nodejs',
+      'Vercel Serverless Functions'
+    ],
+    imgUrl: './images/echovibe.png',
+    url: 'https://echovibe.vercel.app/'
   },
   {
-    name: "University Search Portal (USP)",
-    description: "A simple, responsive web app that allows users to search for universities worldwide using the Hipolabs Universities API. It fetches real-time university data by name and displays key details like university names and domains and also allow users to save universities",
-    tags: ["Html", "Css", "Javascript", "PHP", "MySql"],
-    imgUrl: "./images/university.png",
-    url: "2421962.linux.studentwebserver.co.uk/universitysearchapi/index.php"
+    name: 'University Search Portal (USP)',
+    description:
+      'A simple, responsive web app that allows users to search for universities worldwide using the Hipolabs Universities API. It fetches real-time university data by name and displays key details like university names and domains and also allow users to save universities',
+    tags: ['Html', 'Css', 'Javascript', 'PHP', 'MySql'],
+    imgUrl: './images/university.png',
+    url: '2421962.linux.studentwebserver.co.uk/universitysearchapi/index.php'
   },
   {
-    name: "Judydoesbraids",
-    description: "JUDYDOESBRAIDS is a modern, SEO-optimized single-page website for a premium hair braiding business, styled with elegant black and gold branding. It features responsive design, a service showcase, dynamic gallery, and an interactive booking system. Optimized for performance, branding, and search visibility.",
-    tags: ["Html", "Css", "Javascript", "PHP", "SQL", "Vercel"],
-    imgUrl: "./images/judyhairs.jpg",
-    url: "https://judydoesbraids.com/test"
+    name: 'Judydoesbraids',
+    description:
+      'JUDYDOESBRAIDS is a modern, SEO-optimized single-page website for a premium hair braiding business, styled with elegant black and gold branding. It features responsive design, a service showcase, dynamic gallery, and an interactive booking system. Optimized for performance, branding, and search visibility.',
+    tags: ['Html', 'Css', 'Javascript', 'PHP', 'SQL', 'Vercel'],
+    imgUrl: './images/judyhairs.jpg',
+    url: 'https://judydoesbraids.com/test'
   },
+  {
+    name: 'Nigeria Business Portal',
+    description:'A modern redesign of the Nigeria Business Portal website aimed at improving user experience, visual appeal, and mobile responsiveness. The new design features a clean layout, faster performance, and intuitive navigation, making it easier for users to explore business resources and services. This rebrand enhances the platform’s credibility and positions it for better engagement and growth.',
+    tags: ['Html', 'Css', 'Javascript'],
+    imgUrl: './images/ngieriaportal.jpg',
+    url: 'https://nigeriabusinessportal.vercel.app/'
+  }
 ]
